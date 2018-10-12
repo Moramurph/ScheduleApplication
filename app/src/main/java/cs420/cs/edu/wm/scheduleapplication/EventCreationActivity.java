05cc1a35d0b15a50382c3304d5e83d24db82a09b
Scott Murphy <moramurph@gmail.com>
2018-10-12 15:35:12 -0400

    Pre Mic integration

--- app/src/main/java/cs420/cs/edu/wm/scheduleapplication/EventCreationActivity.java
+++ app/src/main/java/cs420/cs/edu/wm/scheduleapplication/EventCreationActivity.java
@@ -1,21 +1,26 @@
 package cs420.cs.edu.wm.scheduleapplication;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -28,19 +33,47 @@ public class EventCreationActivity extends AppCompatActivity {
 
     private Button submitButton;
     private Button cameraButton;
+    private ImageButton micRecord;
+    private ImageButton micPlay;
+    private ImageButton micPause;
+
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+
     private EditText titleInput;
     private EditText dateInput;
     private EditText descriptionInput;
     private EditText urlInput;
+
     private Bitmap imageBitmap;
     private ImageView pictureImage;
+
     private String title;
     private String date;
     private String description;
     private String url;
-    private String imageFilePath;
+    private static String imageFilePath = null;
+    private static String audioFilePath = null;
+
     private final String TAG = "EventCreation";
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
 
 
     @Override
@@ -52,8 +85,8 @@ public class EventCreationActivity extends AppCompatActivity {
         dateInput = findViewById(R.id.date_input);
         descriptionInput = findViewById(R.id.description_input);
         urlInput = findViewById(R.id.url_input);
+        micRecord = findViewById(R.id.record_button);
 
-        //TODO Make sure that url is using proper https://
         submitButton = findViewById(R.id.submit_button);
         setupSubmitButton();
         pictureImage = findViewById(R.id.picture_preview);
@@ -61,6 +94,8 @@ public class EventCreationActivity extends AppCompatActivity {
         setupCameraButton();
 
     }
+
+
 
     private void setupSubmitButton() {
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -97,9 +132,7 @@ public class EventCreationActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //Bundle extras = data.getExtras();
             imageBitmap = BitmapFactory.decodeFile(imageFilePath);
-            //imageBitmap = (Bitmap) extras.get("data");
             pictureImage.setImageBitmap(imageBitmap);
         }
     }
